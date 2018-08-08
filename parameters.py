e768ab08374c24000f41d76d0a97e79c026d5a85
gdgrant <gdgrant@uchicago.edu>
2018-08-08 10:25:38 -0500

    Changed input parameters to match convolutional layers

--- parameters.py
+++ parameters.py
@@ -26,18 +26,18 @@ par = {
     # Reptile-specific parameters
     'n_meta_tasks'          : 20,
     'n_test_tasks'          : 5,
-    'k_steps'               : 1,
+    'k_steps'               : 5,
     'epsilon'               : 1.,
 
     # Dropout
-    'drop_keep_pct'         : 0.5,
+    'dropout_drop_pct'      : 0.0,
     'input_drop_keep_pct'   : 1.0,
     'conv_drop_keep_pct'    : 0.75,
 
     # Training specs
     'test_batch_size'       : 200,
     'pre_train_batch_size'  : 10,
-    'pre_train_batches'     : int(1e5)*5,
+    'pre_train_batches'     : int(1e5),
     'shot_batch_size'       : 5,
     'eval_iterations'       : 50,
     'testing_repetitions'   : 100,
@@ -60,7 +60,7 @@ def update_dependencies():
         par['n_output'] = 10
     elif par['task'] == 'omniglot':
         par['input_shape'] = [105, 105]
-        par['n_input'] = 2304 if par['conv_input'] else np.product(par['input_shape'])
+        par['n_input'] = 3136 if par['conv_input'] else np.product(par['input_shape'])
         par['n_output'] = par['n_meta_tasks'] + par['n_test_tasks']
 
     par['layer_dims'] = [par['n_input']] + par['hidden_layers'] + [par['n_output']]
