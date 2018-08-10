8777585ae761801b755739a9b1d90e08cd54a3e3
gdgrant <gdgrant@uchicago.edu>
2018-08-10 10:42:25 -0500

    Updated parameters

--- parameters.py
+++ parameters.py
@@ -14,33 +14,33 @@ global par
 par = {
     # General parameters
     'save_dir'              : './savedir/',
+    'load_from_checkpoint'  : False,
     'meta_learning_rate'    : 0.001,
     'task_learning_rate'    : 0.001,
     'task'                  : 'omniglot',
     'conv_input'            : True,
-
-    # Task specs
-    'n_tasks'               : 100,          # Randomly pre-generated MNIST tasks
-    'hidden_layers'         : [400, 400],
+    'dropout_drop_pct'      : 0.0,
+    'hidden_layers'         : [],
 
     # Reptile-specific parameters
-    'n_meta_tasks'          : 20,
-    'n_test_tasks'          : 5,
     'k_steps'               : 5,
+    'k_steps_dummy'         : 1,
+    'n_ways'                : 5,
+    'n_shots'               : 5,
     'epsilon'               : 1.,
 
-    # Dropout
-    'dropout_drop_pct'      : 0.0,
-    'input_drop_keep_pct'   : 1.0,
-    'conv_drop_keep_pct'    : 0.75,
-
     # Training specs
-    'test_batch_size'       : 200,
+    'meta_batch_size'       : 5,
+
     'pre_train_batch_size'  : 10,
-    'pre_train_batches'     : int(1e5),
-    'shot_batch_size'       : 5,
+    'pre_train_batches'     : int(1e4),
+
     'eval_iterations'       : 50,
-    'testing_repetitions'   : 100,
+    'eval_batch_size'       : 5,
+
+    'test_repetitions'      : 1000,
+    'test_iterations'       : 20,
+    'test_batch_size'       : 100,
 
 }
 
@@ -55,13 +55,14 @@ def update_dependencies():
     """
 
     if par['task'] == 'mnist':
+        par['n_tasks'] = 100
         par['input_shape'] = [28, 28]
         par['n_input'] = np.product(par['input_shape'])
         par['n_output'] = 10
     elif par['task'] == 'omniglot':
-        par['input_shape'] = [105, 105]
-        par['n_input'] = 3136 if par['conv_input'] else np.product(par['input_shape'])
-        par['n_output'] = par['n_meta_tasks'] + par['n_test_tasks']
+        par['input_shape'] = [26, 26]
+        par['n_input'] = 256 if par['conv_input'] else np.product(par['input_shape'])
+        par['n_output'] = par['n_ways'] #par['n_meta_tasks'] + par['n_test_tasks']
 
     par['layer_dims'] = [par['n_input']] + par['hidden_layers'] + [par['n_output']]
 
@@ -71,6 +72,8 @@ def update_dependencies():
         par['labels_per_task'] = 10
     elif par['task'] == 'cifar':
         par['labels_per_task'] = 5
+
+
 
 
 def update_parameters(updates):
